cb21b115814836443080ce5d929004ee8738e32b
Akhilesh Bhatia <akhilesh.bhatia@siren.io>
2022-12-17 06:49:13 +0000

    Update readme and add the case where rentNights is null

--- README.md
+++ README.md
@@ -7,9 +7,12 @@
 This app takes an address and radius in kms and scrapes vrbo.com for this information.
 It opens the browser temporarily and closes it once the scraping is done.
 
-It has three APIs
+It has two APIs
 1. getListingsWithinDistance API - creates a `data.csv` with the 50 closest listings, their prices for next 365 days and their distance
 1. getHighestPricedDates API - returns an object which has the listing name and it's 3 dates when the listing is most expensive
+
+NOTE: The highest prices logic takes the highest unique price and picks the 'first' occurence of the price from the rentNights array 
+and calculates that date.
 
 ## Running the app
 1. Clone the repository locally
@@ -22,3 +25,32 @@ It has three APIs
   curl http://localhost:3000/getHighestPricedDates/73-w-monroe-st-chicago-il-60603-usa/2
 ```
 
+```
+  curl http://localhost:3000/getListingsWithinDistance/lake-district-wildlife-park-keswick-england-united-kingdom/2
+  curl http://localhost:3000/getHighestPricedDates/lake-district-wildlife-park-keswick-england-united-kingdom/2
+```
+
+## Missing implementation
+The code is missing the following:
+1. More modularity. Breaking `extract_data` into smaller chunks. Will make it easier for testing and maintaining
+1. Error handling.
+    1. Doesn't verify that address and radius are not empty and are in the correct format
+    1. No try-catch block in case puppeteer fails
+1. Unit tests which mock's puppeteer's response and check if processing works fine
+1. No security like a JWT auth token for the APIs
+## Thought exercise answer
+Scraping is expensive and time consuming. So when a address and a radius are requested, I will store the scraped data for that address and radius in
+a database. That's because reading a database will be faster than the scraping. The data post processing can happen once the data is retrieved from the database.
+
+The columns in the Cached_Info table will be as follows. It will have the address and the radius of the request along with the response in three different columns.
+Additionally, there will be a Request_timestamp column to check when the information was last scraped. If the new request is within 24 hours, then
+we can use the cached information. If not, we scrape again and store the new response and the new timestamp
+
+Table => Cached_Info
+| Column Name | Description |
+| -------- | -------------- |
+| Id | auto generated unique identifier |
+| Address | address of the request |
+| Radius | radius of the request |
+| Request_timestamp | timestamp when the information was last scraped |
+| Response| scraped response from vrbo |
--- extract_data.js
+++ extract_data.js
@@ -5,6 +5,7 @@ const { scrollPageToBottom } = require('puppeteer-autoscroll-down');
 const baseUrl = 'https://www.vrbo.com';
 
 // This function waits for data request url to be fulfilled. Once available, it returns the response
+// It verifies data.data.results is there or not because there can be multiple requests to the same URL but with different responses
 const waitForDataRequestUrl = async (page) => {
   const response = await page.waitForResponse(async response => {
     if (response.url() === `${baseUrl}/serp/g` && response.status() === 200) {
@@ -17,10 +18,16 @@ const waitForDataRequestUrl = async (page) => {
 }
 
 // This function evaluates each page
-const evaluatePage = async (page, origin, idAndDistance, idAndOtherInfo, maxDistance) => {
-  const response = await waitForDataRequestUrl(page);
-  const data = await response.json();
-  data.data.results.listings.map(listing => {
+const evaluatePage = async (page, pageNum, data, origin, idAndDistance, idAndOtherInfo, maxDistance) => {
+  let finalData = data;
+  if (pageNum !== 0) {
+    const response = await waitForDataRequestUrl(page);
+    finalData = await response.json();
+  }
+  else {
+    await page.waitForTimeout(2000);
+  }
+  finalData.data.results.listings.map(listing => {
     const destination = listing.geoCode;
     // Calculate the haversine distance between origin and destination
     const distance = haversine(origin, destination)/1000; // Divide by 1000 to get the distance in kms
@@ -40,7 +47,7 @@ const evaluatePage = async (page, origin, idAndDistance, idAndOtherInfo, maxDist
 // This function takes the two maps and returns an array with name, prices for next 1 year and distance from source
 const getListingsPriceForNext12Months = async (idAndDistance, idAndOtherInfo, page) => {
   const sortedDistanceMap = new Map([...idAndDistance.entries()].sort((a, b) => a[1] - b[1]));
-  const keys = Array.from(sortedDistanceMap.keys()).splice(0, 50);
+  const keys = [...sortedDistanceMap.keys()].splice(0, 50);
   const listingPricesAndDistance = [];
   for (key of keys) {
     const { name, detailsUrl } = idAndOtherInfo.get(key);
@@ -48,10 +55,11 @@ const getListingsPriceForNext12Months = async (idAndDistance, idAndOtherInfo, pa
     // Get the page source
     const content = await page.content();
     // Parse the source string and get the array between `rentNights` and `rentNightsConverted` which is the rent nights array
-    const stringArr = content.substring(content.indexOf('"rentNights":'), content.indexOf(',"rentNightsConverted":')).split(':')[1];
+    const substring = content.substring(content.indexOf('"rentNights":'), content.indexOf(',"rentNightsConverted":')).split(':')[1];
+    const parsed = JSON.parse(substring);
     listingPricesAndDistance.push({ 
       name,
-      prices: JSON.parse(stringArr).splice(0, 365),
+      prices: !!parsed ? parsed.splice(0, 365) : 'Price per day not available', // Some properties have rentNights as null
       distance: Math.round(sortedDistanceMap.get(key) * 100) / 100 
     });
   }
@@ -69,14 +77,16 @@ const getListingDetailsWithinDistance = async (address, maxDistance) => {
   const response = await waitForDataRequestUrl(page);
   const data = await response.json();
   const { pageCount, geography } = data.data.results;
+  console.log(`Total pages: ${pageCount}`);
   // We create two maps and pass them by reference to evaluatePage
   // One map will store the listing id and the distance and the other will store the listing id and info about the listing
   // Two maps because we use the first one to sort by distance. Once sorted, we use the keys from the sorted map to get the info
   const idAndDistance = new Map();
   const idAndOtherInfo = new Map();
   for (let index = 0; index < pageCount; index++) {
+    console.log(`Evaluating page ${index + 1}`);
     // Evaluate each page
-    await evaluatePage(page, geography.location, idAndDistance, idAndOtherInfo, maxDistance);
+    await evaluatePage(page, index, data, geography.location, idAndDistance, idAndOtherInfo, maxDistance);
 
     // If it's not the last page, click the next button
     if (index != (pageCount-1)) {
@@ -95,7 +105,11 @@ const getHighestPricedDates = async (address, distance) => {
   const listingPricesAndDistance = await getListingDetailsWithinDistance(address, distance);
   const listingAndHighestPricedDates = {};
   listingPricesAndDistance.map(listing => {
-    // create a shallow copy of the prices array for sorting as Array.sort updates original array
+    if (typeof listing.prices === 'string') {
+      listingAndHighestPricedDates[listing.name] = 'Price per day not available';
+    }
+    else {
+      // create a shallow copy of the prices array for sorting as Array.sort updates original array
     const pricesArr = listing.prices.slice();
     // use Set to get unique values + javascript Sets preserve insertion order
     const prices = [...new Set(pricesArr.sort((a, b) => b - a))];
@@ -110,6 +124,7 @@ const getHighestPricedDates = async (address, distance) => {
       dates.push(`${dateForPrice.getDate()}/${dateForPrice.getMonth() + 1}/${dateForPrice.getFullYear()}`);
     }
     listingAndHighestPricedDates[listing.name] = dates;
+    }
   });
   return listingAndHighestPricedDates;
 }
